9f92e7b4842fe034a27ad30ced1ff540645e1f30
Paul-Emmanuel Gautreau <paul-emmanuel.gautreau@ign.fr>
2015-05-26 10:24:01 +0200

    ajout appel à bat de osgeo pour set des variables d'environnement

--- test.bat
+++ test.bat
@@ -1,9 +1,10 @@
-set PYTHONPATH=%PYTHONPATH%;C:\OSGeo4W\apps\qgis\python
-set PATH=%PATH%;C:\OSGeo4W\bin
-set PATH=%PATH%;C:\OSGeo4W\apps\qgis\bin
-set PYTHONPATH=%PYTHONPATH%;C:\OSGeo4W\apps\qgis\python\plugins
-set PYTHONPATH=%PYTHONPATH%;%USERPROFILE%\.qgis2\python\plugins
-set PYTHONPATH=%PYTHONPATH%;./
+call c:\OSGeo4W\bin\o4w_env.bat
+set PYTHONPATH=C:\OSGeo4W\apps\qgis\python;%PYTHONPATH%
+set PATH=C:\OSGeo4W\bin;%PATH%
+set PATH=C:\OSGeo4W\apps\qgis\bin;%PATH%
+set PYTHONPATH=C:\OSGeo4W\apps\qgis\python\plugins;%PYTHONPATH%
+set PYTHONPATH=%USERPROFILE%\.qgis2\python\plugins;%PYTHONPATH%
+set PYTHONPATH=./;%PYTHONPATH%
 set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis
 
 python test\src\testFusion.py
