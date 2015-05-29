8abbc0e74cb7538d5ea00ad722fb69ae73f62c9e
Paul-Emmanuel Gautreau <paul-emmanuel.gautreau@ign.fr>
2015-05-29 11:54:16 +0200

    test integration (error à enquêter)

--- test/src/testFusion.py
+++ test/src/testFusion.py
@@ -1,16 +1,22 @@
 from qgis.core import *
 from qgis.gui import *
-
-from PyQt4 import QtCore, QtGui, QtTest
+from PyQt4 import QtCore, QtGui, uic
 
 import unittest
 import os,sys
-
+from qgis_interface import QgisInterface
 #sys.path.append("./")
 
-from mapping_tools import MappingTools
+import MappingTools
+from common import Common
+from PyQt4.Qt import QObject, QTest
 
-
+class DummyCanvas(object):
+    def __getattr__(self, *args, **kwargs):
+        def dummy(*args, **kwargs):
+            return self
+        return dummy
+    
 class DummyInterface(object):
     def __getattr__(self, *args, **kwargs):
         def dummy(*args, **kwargs):
@@ -23,34 +29,61 @@ class DummyInterface(object):
     def layers(self):
         # simulate iface.legendInterface().layers()
         return QgsMapLayerRegistry.instance().mapLayers().values()
-
-
+    def mainWindow(self):
+        return None
+    def mapCanvas(self):
+        return QgsMapCanvas()
+    
+class SignalBox(QtCore.QObject):
+    def __init__(self, *args, **kwargs):
+        QtCore.QObject.__init__(self, *args, **kwargs)
+        self.received = {}
+    def __getattr__(self, key):
+        return partial(self.__slot, key)
+    def __slot(self, attr=None, *args):
+        sig = tuple([self.sender()] + list(args))
+        if attr not in self.received:
+            self.received[attr] = []
+        self.received[attr].append(sig)
 
 class TestFusion(unittest.TestCase):
+
+    message_log = {}
+    
+    def log(self, message, tag, level):
+        self.message_log.setdefault(tag, [])
+        self.message_log[tag].append((message, level,))
+       
 
 
     def test_fonctionnel(self):
         
+        QgsMessageLog.instance().messageReceived.connect(self.log)
+        
         prefix_path = os.environ['QGIS_PREFIX_PATH']
         print "Env QGIS_PREFIX_PATH : {}".format(prefix_path)
+        app = QtGui.QApplication(sys.argv)
         QgsApplication.setPrefixPath(prefix_path, True)  
         QgsApplication.initQgis()
-        
-        iface = DummyInterface()
-         
+        #print app
+        # Get an iface object
+        canvas = QgsMapCanvas()
+        iface = QgisInterface(canvas)
+
+        QgsMapLayerRegistry.instance().addMapLayer(iface.activeLayer())
+        iface.mapCanvas().setCurrentLayer(iface.activeLayer())
+        clayer = iface.mapCanvas().currentLayer()
+        print clayer
         
         QtCore.QCoreApplication.setOrganizationName('QGIS')
         QtCore.QCoreApplication.setApplicationName('QGIS2')
-        
-        plugin = MappingTools
-        
-        
-        
-        #layer = QgsVectorLayer('/home/guillaume/Documents/c++/GIT/qgis_mapping_tools/data/vectors/mailleA-18-C1.shp', 'input', 'ogr')
+
+        self.plugin = MappingTools.classFactory(iface)
+        #self.ui = self.plugin.dlg.ui # useful for shorthand later
+        self.plugin.initGui()
         layer = QgsVectorLayer('test/data/segment.shp', 'input', 'ogr')
         
         if not layer.isValid():
-            
             print "Probleme de chargement du shapefile..."
             print "- Verifiez la liste des provider (need OGR) :"
             providers = QgsProviderRegistry.instance().providerList()
@@ -59,21 +92,40 @@ class TestFusion(unittest.TestCase):
             print "- Verifiez la liste des chemins :"
             print QgsApplication.showSettings()
             self.fail('Failed to load "{} - ".'.format(layer.source()))
+        clayer.startEditing()
+        print clayer.isEditable()
+        iface.mapCanvas().saveAsImage('img.png')
+        #signalbox = SignalBox()
+        #iface.mapCanvas().
+        #cursor = QtGui.QCursor()
+        #iface.mapCanvas().setCursor(cursor)
+        #pos = cursor.pos()
+        #cursor.setPos(pos.x()+1, pos.y()+1)
+        #print cursor
+        #event = QtGui.QMouseEvent(2, QtCore.QPoint(), 1, 0, 0)
+        #qpoint = iface.mapCanvas().getCoordinateTransform().transform(784096.41,6528576.19).toQPointF().toPoint()
+        qpoint = QtCore.QPoint()
+        print isinstance(iface.mapCanvas(), QtGui.QWidget)
+        
+        QgsMessageLog.instance().logMessage("TEST MESSAGE")
+        
+        print self.message_log
+        #QTest.log()
         
         
-        QgsMapLayerRegistry.instance().addMapLayer(layer)
+        QTest.mousePress(iface.mapCanvas(), QtCore.Qt.LeftButton)
+        #QTest.mouseMove(iface.mapCanvas())
+        #QTest.mouseRelease(iface.mapCanvas(), QtCore.Qt.LeftButton)
         
+
         total_area = 0.0
         for feature in layer.getFeatures():
             total_area += feature.geometry().area()
 
-        
         print layer.dataProvider().featureCount()
         print total_area
-        
+
     def test_fonctionnel2(self):
         self.assertTrue(1 == 1, "")
-
 if __name__ == '__main__':
     unittest.main()
-    
